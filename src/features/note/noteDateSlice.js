a40f9418578b079078f50a73012c95a4c281032e
Julio Martins <juliomarttins5@gmail.com>
2021-11-16 14:35:59 -0300

    fix: increment dates
    fix the increment dates error and change the name @jnotes-notes to @jnotes-dates
    
    Thank you :)

--- src/features/note/noteDateSlice.js
+++ src/features/note/noteDateSlice.js
@@ -6,18 +6,21 @@ export const noteDateSlice = createSlice({
   name: "noteDate",
   initialState: {
     dates:
-      store.get("@jnotes-notes") === null ? [] : store.get("@jnotes-notes"),
+      store.get("@jnotes-dates") === undefined ||
+      store.get("@jnotes-dates") === null
+        ? []
+        : store.get("@jnotes-dates"),
   },
   reducers: {
     incrementDates: (state) => {
       state.dates.push(dateFormat(new Date(), "mmm dd, yyyy hh:MM TT"));
-      store.set("@jnotes-notes", state.dates);
+      store.set("@jnotes-dates", state.dates);
     },
     decrementDates: (state, actions) => {
       const arrayPosition = actions.payload;
 
       state.dates.splice(arrayPosition, 1);
-      store.set("@jnotes-notes", state.dates);
+      store.set("@jnotes-dates", state.dates);
     },
   },
 });
